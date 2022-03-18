9900607dd62f340d538a676024b9b232c3bc54cd
adikolo2 <idiomy28>
2022-03-18 12:45:58 +0100

    Small RpsGameTest refactor

--- src/test/java/com/ava/tasks/RpsGameTest.java
+++ src/test/java/com/ava/tasks/RpsGameTest.java
@@ -28,105 +28,120 @@ class RpsGameTest {
 
     @Test
     public void testRockAndScissors() {
+        //arrange
         Mockito.when(this.scanner.nextLine()).thenReturn("r", "s");
-        rpsGame.play();
-        Assertions.assertEquals("*** Starting Rock - Paper - Scissors ***\n" +
-                        "Please type corresponding letter to choose your shape\n" +
-                        "Rock - r\n" +
-                        "Paper - p\n" +
-                        "Scissors - s\n" +
-                        "May the better win!\n" +
-                        "\r\n" +
-                        "Choose the first shape\r\n" +
-                        "You have chosen: ROCK\r\n" +
-                        "Choose the second shape\r\n" +
-                        "You have chosen: SCISSORS\r\n" +
-                        "Fight!\r\n" +
-                        "ROCK wins!\r\n" +
-                        "Thank you for playing!",
-                outputStreamCaptor.toString()
-                        .trim());
+        final var expected = "*** Starting Rock - Paper - Scissors ***\n" +
+                "Please type corresponding letter to choose your shape\n" +
+                "Rock - r\n" +
+                "Paper - p\n" +
+                "Scissors - s\n" +
+                "May the better win!\n" +
+                "\r\n" +
+                "Choose the first shape\r\n" +
+                "You have chosen: ROCK\r\n" +
+                "Choose the second shape\r\n" +
+                "You have chosen: SCISSORS\r\n" +
+                "Fight!\r\n" +
+                "ROCK wins!\r\n" +
+                "Thank you for playing!";
+
+        //act
+        this.rpsGame.play();
+
+        //assert
+        Assertions.assertEquals(expected,this.outputStreamCaptor.toString().trim());
     }
 
     @Test
     public void testPaperAndRock() {
+        //arrange
         Mockito.when(this.scanner.nextLine()).thenReturn("p", "r");
-        rpsGame.play();
-        Assertions.assertEquals("*** Starting Rock - Paper - Scissors ***\n" +
-                        "Please type corresponding letter to choose your shape\n" +
-                        "Rock - r\n" +
-                        "Paper - p\n" +
-                        "Scissors - s\n" +
-                        "May the better win!\n" +
-                        "\r\n" +
-                        "Choose the first shape\r\n" +
-                        "You have chosen: PAPER\r\n" +
-                        "Choose the second shape\r\n" +
-                        "You have chosen: ROCK\r\n" +
-                        "Fight!\r\n" +
-                        "PAPER wins!\r\n" +
-                        "Thank you for playing!",
-                outputStreamCaptor.toString()
-                        .trim());
+        final var expected = "*** Starting Rock - Paper - Scissors ***\n" +
+                "Please type corresponding letter to choose your shape\n" +
+                "Rock - r\n" +
+                "Paper - p\n" +
+                "Scissors - s\n" +
+                "May the better win!\n" +
+                "\r\n" +
+                "Choose the first shape\r\n" +
+                "You have chosen: PAPER\r\n" +
+                "Choose the second shape\r\n" +
+                "You have chosen: ROCK\r\n" +
+                "Fight!\r\n" +
+                "PAPER wins!\r\n" +
+                "Thank you for playing!";
+
+        //act
+        this.rpsGame.play();
+
+        //assert
+        Assertions.assertEquals(expected,this.outputStreamCaptor.toString().trim());
     }
 
     @Test
     public void testPaperAndPaper() {
+        //arrange
         Mockito.when(this.scanner.nextLine()).thenReturn("p", "p");
-        rpsGame.play();
-        Assertions.assertEquals("*** Starting Rock - Paper - Scissors ***\n" +
-                        "Please type corresponding letter to choose your shape\n" +
-                        "Rock - r\n" +
-                        "Paper - p\n" +
-                        "Scissors - s\n" +
-                        "May the better win!\n" +
-                        "\r\n" +
-                        "Choose the first shape\r\n" +
-                        "You have chosen: PAPER\r\n" +
-                        "Choose the second shape\r\n" +
-                        "You have chosen: PAPER\r\n" +
-                        "Fight!\r\n" +
-                        "A truce!\r\n" +
-                        "Thank you for playing!",
-                outputStreamCaptor.toString()
-                        .trim());
+        final var expected = "*** Starting Rock - Paper - Scissors ***\n" +
+                "Please type corresponding letter to choose your shape\n" +
+                "Rock - r\n" +
+                "Paper - p\n" +
+                "Scissors - s\n" +
+                "May the better win!\n" +
+                "\r\n" +
+                "Choose the first shape\r\n" +
+                "You have chosen: PAPER\r\n" +
+                "Choose the second shape\r\n" +
+                "You have chosen: PAPER\r\n" +
+                "Fight!\r\n" +
+                "A truce!\r\n" +
+                "Thank you for playing!";
+        //act
+        this.rpsGame.play();
+
+        //assert
+        Assertions.assertEquals(expected,this.outputStreamCaptor.toString().trim());
     }
 
     @Test
     public void testIncorrectInputs() {
+        //arrange
         Mockito.when(this.scanner.nextLine()).thenReturn("wrong", "p", "wrong", "r");
-        rpsGame.play();
-        Assertions.assertEquals("*** Starting Rock - Paper - Scissors ***\n" +
-                        "Please type corresponding letter to choose your shape\n" +
-                        "Rock - r\n" +
-                        "Paper - p\n" +
-                        "Scissors - s\n" +
-                        "May the better win!\n" +
-                        "\r\n" +
-                        "Choose the first shape\r\n" +
-                        "Please type correct shape\r\n" +
-                        "You have chosen: PAPER\r\n" +
-                        "Choose the second shape\r\n" +
-                        "Please type correct shape\r\n" +
-                        "You have chosen: ROCK\r\n" +
-                        "Fight!\r\n" +
-                        "PAPER wins!\r\n" +
-                        "Thank you for playing!",
-                outputStreamCaptor.toString()
-                        .trim());
+        final var expected = "*** Starting Rock - Paper - Scissors ***\n" +
+                "Please type corresponding letter to choose your shape\n" +
+                "Rock - r\n" +
+                "Paper - p\n" +
+                "Scissors - s\n" +
+                "May the better win!\n" +
+                "\r\n" +
+                "Choose the first shape\r\n" +
+                "Please type correct shape\r\n" +
+                "You have chosen: PAPER\r\n" +
+                "Choose the second shape\r\n" +
+                "Please type correct shape\r\n" +
+                "You have chosen: ROCK\r\n" +
+                "Fight!\r\n" +
+                "PAPER wins!\r\n" +
+                "Thank you for playing!";
+
+        //act
+        this.rpsGame.play();
+
+        //assert
+        Assertions.assertEquals(expected,this.outputStreamCaptor.toString().trim());
     }
 
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        rpsGame = new RpsGame(this.scanner);
-        System.setOut(new PrintStream(outputStreamCaptor));
+        this.rpsGame = new RpsGame(this.scanner);
+        System.setOut(new PrintStream(this.outputStreamCaptor));
     }
 
 
     @AfterEach
     public void tearDown() {
-        System.setOut(standardOut);
+        System.setOut(this.standardOut);
     }
 }
